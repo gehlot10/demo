0269d456959312f254a6317a5f0a3a3e9289ea02
gehlot10 <nikhilvgehlot003@gmail.com>
2024-07-31 14:46:09 +0000

    index.js updated

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const express = require("express")
-var cors = require("cors")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-const start = ()=>{
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-    try{
-        app.listen(PORT, ()=>{
-            console.log("Server is running at port ",PORT)
-        })
-    }
-    catch(error){
-        console.log({err:error})
-    }
-}
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+// Start the server
+const start = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log("Server is running at port", PORT);
+    });
+  } catch (error) {
+    console.log({ err: error });
+  }
+};
+
 start();
-
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
-app.use(cors())
-app.use(express.json())
-
-
