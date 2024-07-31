a0f6f8f440c5f8559e42190a88b2ebc28bd2b59c
gehlot10 <nikhilvgehlot003@gmail.com>
2024-07-31 12:10:45 +0000

    third

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(process.env.API_KEY)
+const express = require("express")
+const app = express()
+app.use(epxress.json())
+
+app.get("/",(req,res)=>{
+    console.log(process.env.API_KEY)
+})
