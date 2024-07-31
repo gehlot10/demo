c865e75e3a1b72ba859d9ce08414b852390f01f0
gehlot10 <nikhilvgehlot003@gmail.com>
2024-07-31 14:41:29 +0000

    index.js update

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express")
+var cors = require("cors")
 const app = express()
-app.use(express.json())
+const PORT = process.env.PORT || 5000;
+
+const start = ()=>{
+
+    try{
+        app.listen(PORT, ()=>{
+            console.log("Server is running at port ",PORT)
+        })
+    }
+    catch(error){
+        console.log({err:error})
+    }
+}
+start();
 
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send("Hello")
 })
+app.use(cors())
+app.use(express.json())
 
-app.listen(8000,(req,res)=>{
-    console.log("Server running")
-})
+
