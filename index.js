2990d9e41d0d074b2fd25bf972b3e3dbba008bcf
gehlot10 <nikhilvgehlot003@gmail.com>
2024-07-31 12:18:28 +0000

    third

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express()
-app.use(epxress.json())
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     console.log(process.env.API_KEY)
+    res.send(process.env.API_KEY)
+})
+
+app.listen(8000,(req,res)=>{
+    console.log("Server running")
 })
